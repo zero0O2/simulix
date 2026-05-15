dfcec894e578e52387df55277a5ebb8b481fa72c
zero0o2 <carlosbatata195@gmail.com>
2026-05-15 16:22:50 -0300

    ajustes

--- frontend/src/contexts/QuestoesProvider.jsx
+++ frontend/src/contexts/QuestoesProvider.jsx
@@ -9,7 +9,7 @@ const QuestoesContext = createContext();
 const QuestoesProvider = ({children}) => {
 
     const {user} = useAuth()
-    const API_URL = import.meta.env.VITE_API || import.meta.env.VITE_API_URL_LOCAL
+    const API_URL = import.meta.env.VITE_API_URL || import.meta.env.VITE_API_URL_LOCAL
 
     const [questoesForUser, setQuestoesForUser] = useState([])
     const [categoriaTypes, setCategoriaTypes] = useState("Todas")
