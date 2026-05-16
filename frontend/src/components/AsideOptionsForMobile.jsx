a0d58d71fe03a1ff2f805eed5b5b21cc2b723a28
zero0o2 <carlosbatata195@gmail.com>
2026-05-16 12:43:20 -0300

    responsividade

--- frontend/src/components/AsideOptionsForMobile.jsx
+++ frontend/src/components/AsideOptionsForMobile.jsx
@@ -21,9 +21,9 @@ console.log(navigateOptionsForQuest)
 
                 {navigateOptionsForQuest != "/home" &&                
                     <div onClick={()=>setNavigateOptionsForQuest("/home")} className="w-full h-full left-0 top-0 p-[10px] flex items-center absolute z-10">
-                        <div onClick={(e)=>e.stopPropagation()} className="backdrop-blur-2xl w-full overflow-hidden gap-[10px] flex flex-col items-center h-[80%] rounded-[20px]">
+                        <div onClick={(e)=>e.stopPropagation()} className="backdrop-blur-2xl w-full overflow-hidden gap-[10px] flex flex-col items-center h-[100%] rounded-[20px]">
                             <img className="absolute z-[-1] w-full h-full object-cover " src="./images/fundo3.png" alt="" />
-                            <div className="absolute z-[-1] w-full h-full backdrop-blur-[30px] backdrop-brightness-45"></div>
+                            <div className="absolute z-[-1] w-full h-full backdrop-blur-[50px] backdrop-brightness-25"></div>
                             <div className="flex w-full p-[10px_20px] justify-between items-center">
                                 <h1 className="text-[17px]">Controle total da sua preparação</h1>
                                 <OptionsForQuest text="Voltar" path={"/home"}/>
