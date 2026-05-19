49ba93728bca7e5faeccea74d8ac5b302eab73f7
zero0o2 <carlosbatata195@gmail.com>
2026-05-19 15:03:05 -0300

    teste

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -3,9 +3,9 @@ import { useAuth } from "../contexts/AuthProvider"
 import LoadCircle from "./LoadCircle"
 
 const PrivateRoute = ({children,loggedIn = "all",local}) => {
-    const {access} = useAuth()
+    const {user,access} = useAuth()
 
-    if(access === null){return <div className="absolute w-[100dvw] h-[100dvh] flex justify-center text-[50px] items-center"><LoadCircle/></div>}
+    if(access === null || (access === true && user === null )){return <div className="absolute w-[100dvw] h-[100dvh] flex justify-center text-[50px] items-center"><LoadCircle/></div>}
 
     if(loggedIn === "all"){return children}
 
