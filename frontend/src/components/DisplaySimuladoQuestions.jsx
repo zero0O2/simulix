ab33e9f406cd7c60cb951ff4d6236863e2b1c6e8
zero0o2 <carlosbatata195@gmail.com>
2026-05-24 13:53:23 -0300

    preparando area de simulado

--- frontend/src/components/DisplaySimuladoQuestions.jsx
+++ frontend/src/components/DisplaySimuladoQuestions.jsx
@@ -12,11 +12,11 @@ const DisplaySimuladoQuestions = () => {
         <>
 
             <div className=" flex-1 w-full">
-                <aside className="w-full relative  h-[40px] justify-between items-center gap-[20px] flex">
+                <aside className="w-full relative h-[40px] justify-between items-center gap-[20px] flex">
 
                     <AsideOptionsForMobile/>
                     
-                    <button onClick={()=> setQuestoesFoco(prev=>!prev)} className="border-1 border-[var(--azulCeu)] w-[40px] h-[40px] flex justify-center items-center absolute right-0 cursor-pointer text-[30px] text-[var(--azulCeu)]">{questoesFoco ? <MdFullscreenExit/> : <MdFullscreen/>}</button>
+                    <button onClick={()=> setQuestoesFoco(prev=>!prev)} className="border-1 border-[var(--azulCeu)] w-[40px] h-[40px] flex justify-center items-center cursor-pointer text-[30px] text-[var(--azulCeu)]">{questoesFoco ? <MdFullscreenExit/> : <MdFullscreen/>}</button>
                 </aside>
 
                 <div className="w-full h-full text-center">
