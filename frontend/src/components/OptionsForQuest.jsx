b2bfd80ee535f85b7259a13f9cda5bd01bbb1bc1
zero0o2 <carlosbatata195@gmail.com>
2026-05-27 10:26:05 -0300

    ajeitando projeto para iniciar o desenvolvimento das configuraçoes

--- frontend/src/components/OptionsForQuest.jsx
+++ frontend/src/components/OptionsForQuest.jsx
@@ -11,8 +11,8 @@ const OptionsForQuest = ({ text, children, path, info }) => {
                 className="bg-[var(--azulEscuro)] flex justify-center items-center gap-[10px] hover:bg-[var(--azulCeu)] relative duration-300 cursor-pointer p-[10px_20px] rounded"
                 onClick={() => setNavigateOptionsForQuest(path)}
             >   
-                <p className={`${children && "max-[470px]:hidden"}`}>{text}</p>
-                <span className="max-[1180px]:hidden max-[470px]:block">{children}</span>
+                <p className={`${children && "max-[500px]:hidden"}`}>{text}</p>
+                <span className="max-[1180px]:hidden max-[500px]:block">{children}</span>
 
                 {info && <>
                     <span className="peer absolute right-[10px]">
