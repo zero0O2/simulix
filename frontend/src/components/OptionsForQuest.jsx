cd91e84fa4e5b3cbd57b8ceb0644c1c04b845193
zero0o2 <carlosbatata195@gmail.com>
2026-05-27 10:37:50 -0300

    ajustes

--- frontend/src/components/OptionsForQuest.jsx
+++ frontend/src/components/OptionsForQuest.jsx
@@ -18,10 +18,10 @@ const OptionsForQuest = ({ text, children, path, info }) => {
                     <span className="peer absolute right-[10px]">
                         <IoInformationCircleOutline className="text-[20px]"/>
                     </span>
-                    <div className="peer-hover:opacity-100 opacity-0 max-w-[350px] duration-300 ease-in-out absolute right-0 top-[100%] bg-[var(--bege)] text-[var(--textBlack)] p-[10px] rounded-[8px]">
+                    <div className=" peer-hover:opacity-100 opacity-0 max-w-[350px] duration-300 ease-in-out absolute right-0 top-[100%] bg-[var(--bege)] text-[var(--textBlack)] p-[10px] rounded-[8px]">
                         {info}
                     </div>
-                </>}
+                </>} 
             </button>
         </>
     )
